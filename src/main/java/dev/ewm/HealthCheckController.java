434ecc62990520ecb01e0018e6f04ba99fc1f793
taeyun1215 <taeyun1215@naver.com>
2023-01-05 21:16:31 +0900

    spring boot init and mariadb setting

---
+++ src/main/java/dev/ewm/HealthCheckController.java
@@ -0,0 +1,13 @@
+package dev.ewm;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @RequestMapping("/healthCheck")
+    public String healthCheck() {
+        return "200 ok";
+    }
+}
---
+++ src/test/java/dev/ewm/EwmApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ewm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
