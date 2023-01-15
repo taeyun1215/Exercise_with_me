107d3b40cc0ec2919259d5d3562eb9cb38775575
taeyun1215 <taeyun1215@naver.com>
2023-01-15 22:03:26 +0900

    feat : mate entity

--- src/main/java/dev/ewm/domain/mate/mate.java
+++ src/main/java/dev/ewm/domain/mate/mate.java
@@ -1,13 +1,13 @@
 package dev.ewm.domain.mate;
 
+import dev.ewm.domain.matePost.matePost;
+import dev.ewm.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -22,5 +22,12 @@ public class mate {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
+    @ManyToOne(targetEntity = matePost.class, fetch = FetchType.LAZY) // 실제로 요청하는 순간 가져오기 위해 LAZY로 사용함.
+    @JoinColumn(name = "id")
+    private matePost matePost;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY) // 실제로 요청하는 순간 가져오기 위해 LAZY로 사용함.
+    @JoinColumn(name = "username")
+    private User user;
 
 }
