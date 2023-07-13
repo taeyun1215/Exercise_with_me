528669686b73351f1278bee38d2e635febf7adcf
taeyun1215 <taeyun1215@naver.com>
2023-07-13 16:41:07 +0900

    fix : github action

--- user-service/src/main/java/user/HealthCheckController.java
+++ user-service/src/main/java/user/HealthCheckController.java
@@ -25,7 +25,7 @@ public class HealthCheckController {
     @GetMapping("/check")
     public String check(HttpServletRequest request) {
         log.info("Server port = {}", request.getServerPort());
-        return String.format("ttttttttt is a message from user Service on PORT %s",
+        return String.format("ttttttttttt is a message from user Service on PORT %s",
                 env.getProperty("local.server.port"));
     }
 }
