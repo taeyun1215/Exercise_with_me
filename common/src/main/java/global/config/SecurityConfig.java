cf11c02a44f24900898f316590b8b98f84c6f0d7
taeyun1215 <taeyun1215@naver.com>
2023-07-20 14:19:21 +0900

    feat : common module import securityConfig.class

--- common/src/main/java/global/config/SecurityConfig.java
+++ common/src/main/java/global/config/SecurityConfig.java
@@ -1,44 +1,30 @@
-//package global.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import user.adapter.out.persistence.UserJpaRepo;
-//import user.adapter.out.persistence.UserPersistenceMapper;
-//import user.global.argumentResolver.LoginArgumentResolver;
-//
-//import java.util.List;
-//
-//@RequiredArgsConstructor
-//@EnableWebSecurity
-//@Configuration
-//public class SecurityConfig implements WebMvcConfigurer {
-//
-//    private final UserJpaRepo userJpaRepo;
-//    private final UserPersistenceMapper userPersistenceMapper;
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        // csrf 보안 설정 끄기
-//        // 토큰 방식, 즉 stateless 기반 인증에선 서버에서 인증 정보를 보관하지 않기 때문에
-//        // csrf 공격에 안전하고 매번 csrf 토큰을 받기 않기 때문에 불필요)
-//        http.csrf().disable();
-//
-//        // 스프링 시큐리티가 세션을 생성하지 않고 기존 세션을 사용하지도 않음(JWT 사용을 위함)
-//        http
-//                .authorizeRequests().antMatchers("/").permitAll();
-//
-//        return http.build();
-//    }
-//
-//    @Override
-//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-//        resolvers.add(new LoginArgumentResolver(userJpaRepo, userPersistenceMapper));
-//    }
-//
-//}
+package global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // csrf 보안 설정 끄기
+        // 토큰 방식, 즉 stateless 기반 인증에선 서버에서 인증 정보를 보관하지 않기 때문에
+        // csrf 공격에 안전하고 매번 csrf 토큰을 받기 않기 때문에 불필요)
+        http.csrf().disable();
+
+        // 스프링 시큐리티가 세션을 생성하지 않고 기존 세션을 사용하지도 않음(JWT 사용을 위함)
+        http
+                .authorizeRequests().antMatchers("/").permitAll();
+
+        return http.build();
+    }
+
+}
--- order-service/src/main/java/order/OrderServiceApplication.java
+++ order-service/src/main/java/order/OrderServiceApplication.java
@@ -1,6 +1,7 @@
 package order;
 
 import global.config.QuerydslConfig;
+import global.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableDiscoveryClient
-@Import(QuerydslConfig.class)
+@Import({QuerydslConfig.class, SecurityConfig.class})
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
