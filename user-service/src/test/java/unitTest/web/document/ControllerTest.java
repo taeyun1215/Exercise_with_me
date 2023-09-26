d6ea684db10309ee3bf2cf425274970277ce8cd5
taeyun1215 <taeyun1215@naver.com>
2023-09-26 15:46:35 +0900

    feat : user-service register api document

---
+++ user-service/src/test/java/unitTest/web/document/ControllerTest.java
@@ -0,0 +1,29 @@
+package unitTest.web.document;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import user.UserServiceApplication;
+import user.adapter.out.persistence.UserResponseMapper;
+import user.application.port.in.usecase.RegisterUserUseCase;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ActiveProfiles("test")
+@SpringBootTest(classes = UserServiceApplication.class)
+public abstract class ControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockBean
+    private RegisterUserUseCase registerUserUseCase;
+
+    @MockBean
+    private UserResponseMapper userResponseMapper;
+
+}
---
+++ user-service/src/test/java/unitTest/web/document/UserRegisterControllerDocumentTest.java
@@ -0,0 +1,45 @@
+package unitTest.web.document;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import user.adapter.in.request.RegisterUserRequest;
+
+import javax.ws.rs.core.MediaType;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class UserRegisterControllerDocumentTest extends ControllerTest {
+
+    @Test
+    @DisplayName("회원 가입 API 명세서")
+    public void testRegisterUser() throws Exception {
+        RegisterUserRequest request = new RegisterUserRequest(
+                "testuser111",
+                "test111!!!",
+                "test111!!!",
+                "testNickname",
+                "010-1234-5678",
+                "testuser@example.com"
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(MockMvcRestDocumentation.document("user/register-user",
+                        PayloadDocumentation.requestFields(
+                                PayloadDocumentation.fieldWithPath("username").description("User's username"),
+                                PayloadDocumentation.fieldWithPath("password").description("User's password"),
+                                PayloadDocumentation.fieldWithPath("confirmPassword").description("User's confirmed password"),
+                                PayloadDocumentation.fieldWithPath("nickname").description("User's nickname"),
+                                PayloadDocumentation.fieldWithPath("phone").description("User's phone number"),
+                                PayloadDocumentation.fieldWithPath("email").description("User's email address")
+                        )));
+    }
+}
