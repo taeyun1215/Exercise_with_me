a39bc83a816ab8afc556fc73d98166486de56fef
taeyun1215 <taeyun1215@naver.com>
2023-10-06 16:55:44 +0900

    feat : common service http client

---
+++ common/src/main/java/global/common/CommonHttpClient.java
@@ -0,0 +1,38 @@
+package global.common;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Component
+public class CommonHttpClient {
+    private final HttpClient httpClient;
+
+    public CommonHttpClient() {
+        httpClient = HttpClient.newBuilder().build();
+    }
+
+    public HttpResponse<String> sendGetRequest(String url) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> sendPostRequest(String url, String body) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
+
