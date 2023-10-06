b0dd1ec356b299f711325400f9e0337e3bb9e9c9
taeyun1215 <taeyun1215@naver.com>
2023-10-07 01:11:43 +0900

    feat : order-aggregation-service getUserIdByAddress

--- order-aggregation-service/src/main/java/order/aggregation/adapter/out/UserServiceAdapter.java
+++ order-aggregation-service/src/main/java/order/aggregation/adapter/out/UserServiceAdapter.java
@@ -1,21 +1,33 @@
 package order.aggregation.adapter.out;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import global.annotation.ExternalSystemAdapter;
 import global.common.CommonHttpClient;
 import lombok.RequiredArgsConstructor;
 import order.aggregation.application.port.out.GetUserPort;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ExternalSystemAdapter
 @RequiredArgsConstructor
 public class UserServiceAdapter implements GetUserPort {
 
     private final CommonHttpClient commonHttpClient;
-    private final String userServiceUrl = "localhost:8000/user-service";
+    private final String userServiceUrl = "user-service";
 
     @Override
     public List<Long> getUserIdByAddress(String address) {
+        String url = String.join("/", userServiceUrl, "users/address", address);
+        try {
+            String jsonResponse = commonHttpClient.sendGetRequest(url).body();
+
+            ObjectMapper mapper = new ObjectMapper();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
