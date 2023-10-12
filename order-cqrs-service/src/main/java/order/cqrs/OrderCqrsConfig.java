fbf471e51006ff2fe6eec1bc2de6cf97f16931d0
taeyun1215 <taeyun1215@naver.com>
2023-10-12 20:54:34 +0900

    feat : order-cqrs-service lay a foundation

---
+++ order-cqrs-service/src/main/java/order/cqrs/OrderCqrsConfig.java
@@ -0,0 +1,9 @@
+package order.cqrs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("global.common")
+public class OrderCqrsConfig {
+}
---
+++ order-cqrs-service/src/main/java/order/cqrs/OrderCqrsServiceApplication.java
@@ -0,0 +1,19 @@
+package order.cqrs;
+
+import global.config.SecurityConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+
+@EnableDiscoveryClient
+@Import({SecurityConfig.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class OrderCqrsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderCqrsServiceApplication.class, args);
+	}
+
+}
