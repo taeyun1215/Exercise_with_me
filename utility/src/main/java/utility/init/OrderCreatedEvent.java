30ccf52d138947eb61669f349fb6a6b6252fe68c
taeyun1215 <taeyun1215@naver.com>
2023-10-16 16:59:22 +0900

    feat : order-service register api test

--- utility/src/main/java/utility/init/OrderCreatedEvent.java
+++ utility/src/main/java/utility/init/OrderCreatedEvent.java
@@ -1,28 +1,66 @@
 package utility.init;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
 public class OrderCreatedEvent {
 
     @TargetAggregateIdentifier
     private final Long orderId;
-
     private final Long userId;
-
     private final List<OrderItemInfo> orderItems;
 
-    @Getter
-    @ToString
-    @AllArgsConstructor
+    public OrderCreatedEvent(Long orderId, Long userId, List<OrderItemInfo> orderItems) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.orderItems = orderItems;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public List<OrderItemInfo> getOrderItems() {
+        return orderItems;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderCreatedEvent{" +
+                "orderId=" + orderId +
+                ", userId=" + userId +
+                ", orderItems=" + orderItems +
+                '}';
+    }
+
     public static class OrderItemInfo {
         private final Long productId;
         private final int count;
+
+        public OrderItemInfo(Long productId, int count) {
+            this.productId = productId;
+            this.count = count;
+        }
+
+        public Long getProductId() {
+            return productId;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @Override
+        public String toString() {
+            return "OrderItemInfo{" +
+                    "productId=" + productId +
+                    ", count=" + count +
+                    '}';
+        }
     }
 }
