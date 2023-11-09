7c64292613891e06ec5a0d65e3c3d06f2be24909
taeyun1215 <taeyun1215@naver.com>
2023-11-09 23:10:06 +0900

    feat : add health check api, springboot application

---
+++ review-service/src/main/java/review/ReviewServiceApplication.java
@@ -0,0 +1,21 @@
+package review;
+
+import global.config.QuerydslConfig;
+import global.config.SecurityConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableDiscoveryClient
+@Import({QuerydslConfig.class, SecurityConfig.class})
+public class ReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServiceApplication.class, args);
+	}
+
+}
